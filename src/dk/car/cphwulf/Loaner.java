2af61850673422290930681f17804100b260d275
thor <thor@cphbusiness.dk>
2020-11-02 10:14:40 +0100

    loaner test

--- src/dk/car/cphwulf/Loaner.java
+++ src/dk/car/cphwulf/Loaner.java
@@ -1,4 +1,17 @@
 package dk.car.cphwulf;
 
 public class Loaner {
+    String name;
+    String gender;
+    int year;
+
+    public Loaner(String name, String gender, int year) {
+        this.name = name;
+        this.gender = gender;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- test/dk/car/cphwulf/LoanerTest.java
+++ test/dk/car/cphwulf/LoanerTest.java
@@ -1,7 +1,18 @@
 package dk.car.cphwulf;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LoanerTest {
+
+    @Test
+    public void getNameTest() {
+        //5,Ida Overgaard Thomassen,2005,DGI Nordjylland,32.76,1:09.96,1:47.98,2:24.99
+        Loaner loaner = new Loaner("Ida Overgaard","F",2005);
+        String expected = "Kurt";
+        String actual = loaner.getName();
+        assertEquals(expected,actual);
+    }
 
 }
